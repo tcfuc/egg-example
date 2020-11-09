205c8a1a7c497d04045e81a840890945d5238c80
zhou <1141614570@qq.com>
2020-11-09 17:24:53 +0800

    删除无用代码

--- app/controller/spider.js
+++ app/controller/spider.js
@@ -7,11 +7,15 @@ class SpiderController extends Controller {
     const { ctx } = this;
     let newUrls = ['https://www.xuexi.cn/lgdata/1jscb6pu1n2.json?_st=26605838', 'https://www.xuexi.cn/lgdata/u1ght1omn2.json?_st=26618821', 'https://www.xuexi.cn/lgdata/slu9169f72.json?_st=26618868', 'https://www.xuexi.cn/lgdata/tuaihmuun2.json?_st=26618885', 'https://www.xuexi.cn/lgdata/1ajhkle8l72.json?_st=26618885'];
     
+    //获取新闻列表
     let newsJson = await ctx.service.spider.getXueXiUrls(newUrls);
+    //格式化数据并实现条件筛选
     let news = await ctx.service.spider.getXueXiNews(newsJson, 300, '2020-08-01 00:00:00');
+    //获取新闻内容
     let contents = await ctx.service.spider.getNewsContent(news);
     console.log(contents.length);
     ctx.body = contents;
+    //添加入数据库
     await ctx.service.spider.addXueXiNews(contents);
   }
 }
